68640efb5460851008ab2f87cd739fadda8f0eb0
Evan Azevedo <evan@azevedo.com>
2020-08-21 21:20:59 -0700

    some bugs to fix

--- txn-mempool/src/whale_watching.py
+++ txn-mempool/src/whale_watching.py
@@ -68,6 +68,7 @@ async def listen(api_key):
                 try:
                     # the response from the websocket
                     response = await asyncio.wait_for(websocket.recv(), timeout=25)
+                    print(response)
                 except Exception as e:
                     logger.error(str(e))
                     try:
